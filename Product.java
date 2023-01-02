24604f9c4cc082553820994ad7e85ddcb6474065
Programmerazmira <azmira.sultana111@gmail.com>
2023-01-02 19:15:20 +0600

    starting java

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    public static void main(String[] args) {
+        System.out.println("category: phone");
+        System.out.println("phone_id: 101");
+        System.out.println("title: iphone14");
+        System.out.println("price: 1064 euros");
+        System.out.println("description: perfect product with best image quality");
+    }
+}
---
+++ beginner.java
@@ -0,0 +1,9 @@
+
+ class beginner {
+
+    public static void main(String[] args) {
+        System.out.println("Azmira");
+        System.out.print("null");
+        
+    }
+}
