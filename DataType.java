5c1cdfd4ae9d201c5fd77c0aeb044f17d6b05f59
Programmerazmira <azmira.sultana111@gmail.com>
2023-01-04 19:06:17 +0600

    data type printf

--- DataType.java
+++ DataType.java
@@ -2,16 +2,26 @@ public class DataType {
     public static void main(String[] args) {
         boolean b =true;
         char x='a';
-        short Short=32677;
+        short s=32677;
         int i=669869684;
         float f=1.635f;
         double d=50.2654258458;
-        System.out.println("b= "+b);
-        System.out.println("x= "+x);
-        System.out.println("s= "+Short);
-        System.out.println("int= "+i);
-        System.out.println("float= "+f);
-        System.out.println("double= "+d);
+        // System.out.println("b= "+b);
+        // System.out.println("x= "+x);
+        // System.out.println("s= "+Short);
+        // System.out.println("int= "+i);
+        // System.out.println("float= "+f);
+        // System.out.println("double= "+d);
+
+
+
+
+      System.out.printf("boolean = %b\n", b);
+      System.out.printf("char=%c\n",x);
+      System.out.printf("short = %d\n",s);
+      System.out.printf("int= %d\n",i);
+      System.out.printf("float= %f\n",f);
+      System.out.printf("double = %f\n",d);
 
 
 
