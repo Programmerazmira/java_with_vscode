6ca742076044fba9c83fdc741e8ebb00e0789b9c
Programmerazmira <azmira.sultana111@gmail.com>
2023-01-04 18:20:05 +0600

    data type

---
+++ DataType.java
@@ -0,0 +1,20 @@
+public class DataType {
+    public static void main(String[] args) {
+        boolean b =true;
+        char x='a';
+        short Short=32677;
+        int i=669869684;
+        float f=1.635f;
+        double d=50.2654258458;
+        System.out.println("b= "+b);
+        System.out.println("x= "+x);
+        System.out.println("s= "+Short);
+        System.out.println("int= "+i);
+        System.out.println("float= "+f);
+        System.out.println("double= "+d);
+
+
+
+
+    }
+}
--- Product.java
+++ Product.java
@@ -5,5 +5,23 @@ public class Product {
         System.out.println("title: iphone14");
         System.out.println("price: 1064 euros");
         System.out.println("description: perfect product with best image quality");
+
+
+        int id =101;
+        String c,t,p,d;
+        c= "phone";
+        t= "iphone14";
+        p= "1064 euros";
+        d= "description: perfect product with best image quality";
+        System.out.println("id= "+id);
+        System.out.println("title= "+c);
+        System.out.println("title= "+t);
+        System.out.println("title= "+p);
+        System.out.println("title= "+d);
+        
+
+
+
+
     }
 }
